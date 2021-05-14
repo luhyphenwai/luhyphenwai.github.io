1fc7f04de98f1cdf5467df192ffa523dfe2a4690
luhyphenwai <luwaiwong@gmail.com>
2021-05-14 13:46:24 -0400

    in the game baby

--- index.js
+++ index.js
@@ -2,11 +2,15 @@
 window.onscroll = function() {
     scrollFunction();
     section2();
+    section3();
 };
 
-
+scrollFunction();
+section2();
+section3();
 // Navbar animations
 function scrollFunction(){
+    
     let line = document.getElementById("line");
     let topBar = document.getElementById("topBar");
     let leftItem = document.getElementById("leftText");
@@ -15,30 +19,44 @@ function scrollFunction(){
     let right2 = document.getElementById("right2");
     let right3 = document.getElementById("right3");
     if (document.body.scrollTop > 80|| document.documentElement.scrollTop > 80) {
-    line.className = "line-min";
-    topBar.className = "topbar-min";
-    leftItem.className = "left-text-min";
-    rightBox.className = "right-box-min";
-    right1.className = "right-item-min";
-    right1.style.color = "rgba(0,0,0,0)";
-    right1.style.pointerEvents = "none";
-    right2.className = "right-item-min";
-    right2.style.color = "rgba(0,0,0,0)";
-    right2.style.pointerEvents = "none";
-    right3.className = "material-icons right-item-min";
+        line.className = "line-min";
+        topBar.className = "topbar-min";
+        leftItem.className = "left-text-min";
+        rightBox.className = "right-box-min";
+        right1.className = "right-item-min";
+        right1.style.color = "rgba(0,0,0,0)";
+        right1.style.pointerEvents = "none";
+        right2.className = "right-item-min";
+        right2.style.color = "rgba(0,0,0,0)";
+        right2.style.pointerEvents = "none";
+        right3.className = "material-icons right-item-min";
     }
-    else {
-    line.className = "line";
-    topBar.className = "topbar";
-    leftItem.className = "left-text";
-    rightBox.className = "right-box";
-    right1.className = "right-item";
-    right1.style.color = "rgba(0,0,0,1)";
-    right1.style.pointerEvents = "";
-    right2.className = "right-item";
-    right2.style.color = "rgba(0,0,0,1)";
-    right2.style.pointerEvents = "";
-    right3.className = "material-icons right-item";
+    else{
+        line.className = "line";
+        topBar.className = "topbar";
+        leftItem.className = "left-text";
+        rightBox.className = "right-box";
+        right1.className = "right-item";
+        right1.style.color = "rgba(0,0,0,1)";
+        right1.style.pointerEvents = "";
+        right2.className = "right-item";
+        right2.style.color = "rgba(0,0,0,1)";
+        right2.style.pointerEvents = "";
+        right3.className = "material-icons right-item";
+    }
+
+    if (window.location.href.indexOf("/index.html") == -1){
+        line.className = "line-min";
+        topBar.className = "topbar-min";
+        leftItem.className = "left-text-min";
+        rightBox.className = "right-box-min";
+        right1.className = "right-item-min";
+        right1.style.color = "rgba(0,0,0,0)";
+        right1.style.pointerEvents = "none";
+        right2.className = "right-item-min";
+        right2.style.color = "rgba(0,0,0,0)";
+        right2.style.pointerEvents = "none";
+        right3.className = "material-icons right-item-min";
     }
 }
 
